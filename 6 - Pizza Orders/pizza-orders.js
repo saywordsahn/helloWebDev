aa87aa2ac5056e8d52ac2c551ddaec3add9291b3
BuildTools <unconfigured@null.spigotmc.org>
2021-10-17 16:45:53 +0900

    fixed typo

--- 6 - Pizza Orders/pizza-orders.js
+++ 6 - Pizza Orders/pizza-orders.js
@@ -1,7 +1,7 @@
 // We are selling pizzas and need to track who gets the next pizza baked.
 // We are given an array of operations that contains either a character 'c' for customer or a character 'b' for when
 // a pizza is baked.
-// if the operation is 'c' then we take the customers name put it in the queue.
+// if the operation is 'c' then we take the customer's name and put it in the queue.
 // if the operation is 'b' then we give a pizza to the customer that arrived first
 // the operations are read in order
 
