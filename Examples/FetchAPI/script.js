0483c4e45f5e77f7bfaba8900936db8dbad97e4f
TCC Tutors <saywordsahn@gmail.com>
2021-11-02 18:39:46 +0900

    Started some fetch material

---
+++ Examples/FetchAPI/script.js
@@ -0,0 +1,6 @@
+let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+let response = await fetch(url);
+
+let commits = await response.json(); // read response body and parse as JSON
+
+console.log(commits[0].author.login);
